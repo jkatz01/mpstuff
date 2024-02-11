1623d0fe413a8860d9695ec36fa1509e9b739e2e
Yehonatan Katz <yehonatankatz@gmail.com>
2024-02-11 18:20:22 -0500

    still not working

--- A2.c
+++ A2.c
@@ -56,7 +56,6 @@ void print_array(int* data, int size, const char* prefix) {
 int find_b_index(int max_a, int* data_b, int start, int b_size) {
 	// uses binary search to find the greated j
 	// that is smaller or equal to max_a
-	printf("Max: %d Start: %d Size: %d", max_a, start, b_size);
 	int middle, low, high;
 	low = start; // start is the previous j()
 	high = b_size - 1;
@@ -74,9 +73,10 @@ int find_b_index(int max_a, int* data_b, int start, int b_size) {
 		}
 	}
 
+	// Cover duplicates
 	while (low < b_size) {
 		if (data_b[low] <= max_a) {
-			printf("\neq\n");
+			//printf("\neq\n");
 			low++;
 		}
 		else {
@@ -86,8 +86,34 @@ int find_b_index(int max_a, int* data_b, int start, int b_size) {
 
 	// this returns -1 if there is no element
 	// in B that is <= max_a
-	printf("Low: %d\n", low-1);
 	return low-1;
+}
+
+void merge_arrays(const int* data_a, const int* data_b, int* data_c, int a_size, int b_size) {
+	int k = 0;
+	int i = 0;
+	int j = 0;
+	while (k < a_size + b_size) {
+		if (i >= a_size) {
+			data_c[k] = data_b[j];
+			j++;
+		}
+		else if (j >= b_size) {
+			data_c[k] = data_b[i];
+			i++;
+		}
+		else if (data_a[i] <= data_b[j]) {
+			data_c[k] = data_a[i];
+			i++;
+		}
+		else {
+			data_c[k] = data_b[j];
+			j++;
+		}
+		printf("E: %d ", data_c[k]);
+		k++;
+	}
+	printf("\n");
 }
 
 int main (int argc, char *argv[]) {
@@ -99,11 +125,14 @@ int main (int argc, char *argv[]) {
 	int	k;
 	int*	data_a;
 	int*	data_b;
+	int*	data_c;
 	int*	data_a_sizes;
 	int*	data_b_sizes;
+	int*	data_c_sizes;
 	int*	data_a_indices;
 	int*	data_b_indices; //equivalent to j() in algorithm
 	int*	data_b_displs;
+	int*	data_c_displs;
 
 	if (argc != 2) {
 		data_size = 10;
@@ -126,6 +155,8 @@ int main (int argc, char *argv[]) {
 	data_b_sizes   = (int*)malloc(num_procs * sizeof(int));
 	data_b_indices = (int*)malloc(num_procs * sizeof(int));
 	data_b_displs  = (int*)malloc(num_procs * sizeof(int));
+	data_c_sizes   = (int*)malloc(num_procs * sizeof(int));
+	data_c_displs  = (int*)malloc(num_procs * sizeof(int));
 	
 	if (my_rank == FIRST) {
 
@@ -163,10 +194,18 @@ int main (int argc, char *argv[]) {
 			data_b_sizes[i] = data_b_indices[i] - prev;
 			prev = data_b_indices[i];
 		}
-
 		
-		print_array(data_b_indices, num_procs, "j() indices: ");
-		print_array(data_b_sizes, num_procs, "j() sizes: ");
+		// data c sizes
+		int previous_c_displs = 0;
+		for (i = 0; i < num_procs; i++) {
+			int cur_size = data_a_sizes[i] + data_b_sizes[i];
+			data_c_sizes[i] = cur_size;
+			data_c_displs[i] = previous_c_displs;
+			previous_c_displs += cur_size;
+		}
+		
+		//print_array(data_b_indices, num_procs, "j() indices: ");
+		//print_array(data_b_sizes, num_procs, "j() sizes: ");
 
 	}
 	
@@ -176,24 +215,42 @@ int main (int argc, char *argv[]) {
 	
 	int my_a_size = data_a_sizes[my_rank];
 	int my_b_size = data_b_sizes[my_rank];
+	int my_c_size = my_a_size + my_b_size;
 	int* recv_a_buffer = (int*)malloc(my_a_size);
 	int* recv_b_buffer = (int*)malloc(my_b_size);
 
 	MPI_Scatterv(data_a, data_a_sizes, data_a_indices, MPI_INT, recv_a_buffer, my_a_size, MPI_INT, FIRST, MPI_COMM_WORLD);
 	MPI_Scatterv(data_b, data_b_sizes, data_b_displs, MPI_INT, recv_b_buffer, my_b_size, MPI_INT, FIRST, MPI_COMM_WORLD);
 	MPI_Barrier(MPI_COMM_WORLD);
+	// I believe we can free arrays A and B now
 
 	printf("[%d]: ", my_rank);
 	print_array(recv_a_buffer, my_a_size, "My A: ");
 	printf("[%d]: ", my_rank);
 	print_array(recv_b_buffer, my_b_size, "My B: ");
 
+	int* my_data_c = (int*)malloc(my_c_size * sizeof(int));
+	merge_arrays(recv_a_buffer, recv_b_buffer, my_data_c, my_a_size, my_b_size);
+	print_array(my_data_c, my_c_size, "My C: ");
+
+	if (my_rank == FIRST) {
+		// only the first element needs a valid receive buffer
+		data_c = (int*)malloc((data_size * 2) * sizeof(int)); // A and B are same length
+	}
+	MPI_Gatherv(my_data_c, my_c_size, MPI_INT, data_c, data_c_sizes, data_c_displs, MPI_INT, FIRST, MPI_COMM_WORLD);
+
+	if (my_rank == FIRST) {
+		print_array(data_c_sizes, num_procs, "C sizes: ");
+		print_array(data_c_displs, num_procs, "C displs: ");
+		print_array(data_c, data_size*2, "C: ");
+	}
 	
 	MPI_Finalize();
 
 	return 0;
 
 	// TODO: use proper logging instead of printing to console
+	// TODO: free arrays as soon as possible when appropriate
 
 	// Partition A into r groups
 	// each with k = logn elements
